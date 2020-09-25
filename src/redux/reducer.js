d2fb4030033c1debf6635e4652b0ce671e84eaa6
Frank Neugebauer <fneugebauer2012@gmail.com>
2020-09-25 10:38:38 -0400

    React Redux Form/ Form Validation

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,5 +1,5 @@
 import { CAMPSITES } from '../shared/campsites';
-import { COMMENTS } from '../shared/partners';
+import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
 import { PROMOTIONS } from '../shared/promotions';
 
