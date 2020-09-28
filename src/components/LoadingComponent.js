df747453966764855959b330dc7aee5ed0077891
Frank Neugebauer <fneugebauer2012@gmail.com>
2020-09-28 10:29:41 -0400

    Redux Thunk

---
+++ src/components/LoadingComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Loading = () => {
+    return (
+        <div classname="col">
+            <i className="fa fa-spinner fa-pulse fa-3x fa-fw text-primary" />
+            <p>Loading...</p>
+        </div>
+    );
+};
