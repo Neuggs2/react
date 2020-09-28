849b6db2c6dbe81c5b7183e291a3999c1d4e3ffe
Frank Neugebauer <fneugebauer2012@gmail.com>
2020-09-28 08:28:47 -0400

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -28,8 +28,8 @@ class CommentForm extends Component{
     }
 
     handleSubmit(values) {
-        console.log("current state is: " + JSON.stringify(values));
-        alert("Current state is: " + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
 
     }
 
@@ -118,13 +118,13 @@ function RenderCampsite({campsite}) {
     );
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
     if(comments) {
         return (
             <div className='col-md-5 m-1'>
                 <h4>Comments</h4>
                 {comments.map(comments => <div key={comments.id}><div>{comments.text}</div><p>-- {comments.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comments.date)))}</p></div>)}
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>
         );
     }
@@ -148,8 +148,14 @@ function CampsiteInfo(props) {
                         <hr />
                     </div>
                 </div>
-                <div className="row"><RenderCampsite campsite={props.campsite} />
-                <RenderComments comments={props.comments} /></div>
+                <div className="row">
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
+                </div>
             </div>
         );
     } else {
